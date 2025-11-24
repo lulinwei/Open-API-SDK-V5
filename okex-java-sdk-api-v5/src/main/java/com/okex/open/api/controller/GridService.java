a40b979a5fa4c5addae9af8e4f7a7283f1ae59b3
administrator <296691788@qq.com>
2025-11-24 09:37:02 +0800

    1

--- okex-java-sdk-api-v5/src/main/java/com/okex/open/api/controller/GridService.java
+++ okex-java-sdk-api-v5/src/main/java/com/okex/open/api/controller/GridService.java
@@ -63,9 +63,9 @@ public class GridService {
             //整体盈利 一键平仓
             PositionsResponse.DataDTO position = data.get(0);
             log.info("仓位信息：{}", JSON.toJSONString(position));
-            Double liqPx = Double.valueOf(position.getLiqPx());
+            Double liqPx = Double.valueOf(("").equals(position.getLiqPx())?"10000":position.getLiqPx());
             Double bePx = Double.valueOf(position.getBePx());
-            if (Double.valueOf(position.getUpl())+Double.valueOf(position.getRealizedPnl()) > 10) {
+            if (bePx - currentPrice > 50 && Double.valueOf(position.getUpl()) + Double.valueOf(position.getRealizedPnl()) > 10) {
                 ClosePositions closePositions = new ClosePositions();
                 closePositions.setInstId(instId);
                 closePositions.setPosSide("long");
@@ -193,7 +193,7 @@ public class GridService {
         config.setxSimulatedTrading("1");
 
 
-        config.setPrint(true);
+        config.setPrint(false);
         /* config.setI18n(I18nEnum.SIMPLIFIED_CHINESE);*/
         config.setI18n(I18nEnum.ENGLISH);
         return config;
---
+++ okex-java-sdk-api-v5/src/main/java/com/okex/open/api/controller/Task.java
@@ -0,0 +1,24 @@
+package com.okex.open.api.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+@Slf4j
+@EnableScheduling
+@Component
+public class Task {
+
+    @Resource
+    private GridService gridService;
+
+    @Scheduled(fixedRate = 60000) // Execute every 60 seconds (1 minute)
+    public void executeTask() {
+        log.info("Task executed at: " + new java.util.Date());
+        // Add your task logic here
+        gridService.getGrid();
+    }
+}
